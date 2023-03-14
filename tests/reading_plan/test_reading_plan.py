9f1c90035d7a74e88e077e66d410f017e20ef2e0
Raynara Santiago <raysantiagobz@gmail.com>
2023-03-14 16:15:56 -0300

    req 06

--- tests/reading_plan/test_reading_plan.py
+++ tests/reading_plan/test_reading_plan.py
@@ -1,5 +1,15 @@
-from tech_news.analyzer.reading_plan import ReadingPlanService  # noqa: F401, E261, E501
+import pytest
+
+from tech_news.analyzer.reading_plan import ReadingPlanService
+from tests.assets.news import NEWS
 
 
-def test_reading_plan_group_news():
-    pass
+def test_reading_plan_group_news(mocker):
+    mocker.patch(
+                 'tech_news.analyzer.reading_plan.find_news',
+                 return_value=NEWS)
+    news = ReadingPlanService.group_news_for_available_time(3)
+    assert len(news['readable']) == 4
+    assert len(news['unreadable']) == 4
+    with pytest.raises(ValueError):
+        ReadingPlanService.group_news_for_available_time(-4)
